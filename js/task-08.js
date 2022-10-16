bc2da6722ac4dfc4510d6be1badd804ea8178084
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-16 16:46:05 +0300

    Виконує завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+// вибірка елемента
+const form = document.querySelector('.login-form');
+
+// підписка елемента
+form.addEventListener('submit', onFormSubmit);
+
+// обробка елемента
+function onFormSubmit(event) {
+    // відміна перезавантаження сторінки
+    event.preventDefault();
+
+// збір значення полів в об'єкт
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+// попередження при незаповненому полі
+    if (email.value === "" || password.value === "") {
+    return alert("Будь-ласка, заповніть усі поля!");
+    }
+// Вивід у консоль і очиста значення полів
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+
+};
