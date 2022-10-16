18f69baa2b53ac9dcd0b0b7bf713928c5c78f804
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-16 17:22:12 +0300

    Виконує завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// вибірка елементів
+const btn = document.querySelector('button');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+// підписка елемента
+btn.addEventListener('click', changeColor);
+
+  // обробка необхідних елементів
+function changeColor(event) {
+  // зміна кольору body
+  body.style.backgroundColor = getRandomHexColor();
+  // виведення значення кольору
+  span.textContent = body.style.backgroundColor;
+};
